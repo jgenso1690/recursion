e7c7ae2c4291b85f88f77c4a114f834e982b3bcb
jgenso1690 <jimenagensollen@gmail.com>
2020-03-15 19:43:27 -0400

    console to check what we are working on

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,7 +6,7 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-	var doc = document.body;
+	var doc = document.className;
 	var allclassName = [];
 
 	console.log(doc)
