9b3727610b406514893fef270b39f6eb844ab752
jgenso1690 <jimenagensollen@gmail.com>
2020-03-16 03:01:13 -0400

    stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+ 
+	if (type(obj) === "Null") {
+      return null;
+    }
+	if (type(obj) === "Undefined") {
+      return null;
+    }
+	if (type(obj) === "String"){
+	  return '"'+obj+'"';
+    }
+
+	if (typo(obj) === "Number") {
+	  return obj;
+    }
+
+    if (type(obj) === "Array"){
+	  return '['+obj.map(function(el){
+		return stringify(el)
+	  }).join(",") + ']'
+    }
+
+	if (type(obj) === "Object"){
+		var string = []
+		Object.keys(obj).forEach(function(key) {
+			var value = stringify(obj[key])
+		
+		if ( string !== null) {
+			result.push('"'+key+'":' + val)
+		}
+
+	})
+	
+	return "{"+string.join(",")+"}";
+  }
+
 };
