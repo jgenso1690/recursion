6da3fa2b0e5e20a141c029364ba203a6ae62c799
jgenso1690 <jimenagensollen@gmail.com>
2020-03-16 10:53:49 -0400

    getelement

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -20,34 +20,3 @@ var getElementsByClassName = function(className
 	})	
 return result
 }
-	/*if. el === null
-		el.Nodechildren.each {
-		if el.child.className == className {
-			result << el.child
-			
-		}
-		getElementsByClassName(el.child)
-	}
-
-		end 
-		
-	//console.log(list)
-  // your code here
-};
-
-
-2
-<body className = '1'>
-	<h1 className = '2'>
-		<p className = '1'>"lslads" </p>
-	</h1>
-	h2
-<body>
-
-
-body.childNodes
-element.childNodes
-element.children -> property of an element
-
-document.childNodes.length -> how many childnodes theres are
-*/
