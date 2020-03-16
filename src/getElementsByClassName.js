5a5ca7be199108b5f119fe23fb72bf54c954c2b2
jgenso1690 <jimenagensollen@gmail.com>
2020-03-15 21:52:38 -0400

    Revert "console to check what we are working on"
    This reverts commit 8a89f61637353fa088f3c9518ef806515be96a90.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,41 +8,11 @@ var getElementsByClassName = function(className
 ) {
 	
 	
-  var arr = [];
-  var body = document.body;
-  var parent = body;
-  // console.log('current: ' )
-  if (parent.classList.contains(className)) {
-    arr.push(parent);
-  }
-  function loop(parent) {
+	var jdd =document.body.classList
+	var vytfy = document.body.childNodes
 
-    if (!arr.includes(parent) && parent.classList.contains(className)) {
-      arr.push(parent);
-    }
 
-    var children = Array.from(parent.childNodes).filter(item => item.nodeType != Node.TEXT_NODE);
-
-    if (children.length !== 0 || children !== undefined) {
-      for (let i = 0; i < children.length; i ++) {
-
-        if (children[i].classList.contains(className)) {
-          arr.push(children[i]);
-        }
-
-        var grandchildren = Array.from(children[i].childNodes).filter(item => item.nodeType != Node.TEXT_NODE);
-        if (grandchildren.length !== 0 && grandchildren !== undefined) {
-          for (let j = 0; j < grandchildren.length; j ++) {
-            loop(grandchildren[j]);
-          }
-        }
-      }
-
-    }
-
-  }
-  loop(body);
-  return arr;
-}
+	console.log(ele)
+	//console.log(list)
   // your code here
-
+};
